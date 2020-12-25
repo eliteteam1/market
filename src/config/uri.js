d4f932105c464b62df0f90ba1427747e0b2b6854
哎呦 <123@qq.com>
2020-12-25 16:38:48 +0800

    IU

---
+++ src/config/uri.js
@@ -0,0 +1,15 @@
+// 指定代理的前缀
+let prefix = 'https://www.fastmock.site/mock/ed6aa103526d0aea1379e5df9bb4b6d1/v1'
+ 
+// 导出
+export default{
+    // 获取商品列表
+    getGoods: prefix+'goods',
+    // 正在热映
+    // getNowPlaying: prefix+'getNowPlayingFilmList',
+    // 即将上映
+    // getComingSoon: prefix+'getComingSoonFilmList',
+    // 电影详情页面
+    // getFilm:prefix + 'getFilmInfo',
+    // ...
+}
